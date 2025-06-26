b202720e53e8a47e671dcaad1d7fe325bd3123dc
SYWOONH <gkatndus786@naver.com>
2025-06-26 12:01:08 +0900

    Update naver/main.py with latest changes

--- naver/main.py
+++ naver/main.py
@@ -1,5 +1,5 @@
-import crawler_bs42
-import date_finder2
+import crawler_bs4
+import date_finder
 
 
 print("종목코드를 입력하세요 (예: 005930): ", end="")
@@ -13,9 +13,9 @@ start_date = input()
 print("끝날짜 (예: 2023.12.15): ", end="")
 end_date = input()
 
-start, end, url = date_finder2.dfinder(start_date, end_date, stock_code)
+start, end, url = date_finder.dfinder(start_date, end_date, stock_code)
 print(f"{start}에서 {end} 페이지 까지")
 
-crawler_bs42.CWBS(start, end, url, start_date, end_date, stock_code, start_date) #crawler_bs4파일의 CWBS함수에 전달
+crawler_bs4.CWBS(start, end, url, start_date, end_date, stock_code, start_date) #crawler_bs4파일의 CWBS함수에 전달
     
 print("크롤링 끝 \n")
