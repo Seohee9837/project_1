382d08bf9ab8b0a440e76b5a1d99ca2db16ca271
hyeogju <gurwn321@gmail.com>
2025-07-02 15:45:26 +0900

    all

--- hotstock-backend/jobs/go.py
+++ hotstock-backend/jobs/go.py
@@ -3,11 +3,11 @@ from news_collector import collect_news_keywords
 
 def main():
     print("✅ 종토방 키워드 수집 시작...")
-    collect_forum_keywords(10)
+    collect_forum_keywords(None)
     print("👉 종토방 키워드 수집 완료")
 
     print("✅ 뉴스 키워드 수집 시작...")
-    collect_news_keywords(10)
+    collect_news_keywords(None)
     print("👉 뉴스 키워드 수집 완료")
 
 if __name__ == "__main__":
